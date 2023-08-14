f366638062168cfaae25625d40d91195833fc620
Julian Lasso <julian.lasso@sofka.com.co>
2023-08-13 23:29:08 -0500

    chore: :heavy_plus_sign: instalación y configuración de babel-plugin-module-resolver
    Esto es para poder usar los alias en los paths

--- babel.config.js
+++ babel.config.js
@@ -5,4 +5,37 @@ module.exports = {
       plugins: ['react-native-config-node/transform'],
     },
   },
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['./src/'],
+        alias: {
+          '@assets': './src/infrastructure/assets',
+          '@components': './src/infrastructure/components',
+          '@configs': './src/infrastructure/configs',
+          '@routes': './src/infrastructure/routes',
+          '@store': './src/infrastructure/store',
+          '@utils': './src/infrastructure/utils',
+          '@module/notifications': './src/infrastructure/modules/notifications',
+          '@module/reports': './src/infrastructure/modules/reports',
+          '@module/security': './src/infrastructure/modules/security',
+          '@module/transactions': './src/infrastructure/modules/transactions',
+          '@application': './src/application',
+          '@domain': './src/domain',
+          '@infrastructure': './src/infrastructure',
+        },
+      },
+    ],
+  ],
 };
